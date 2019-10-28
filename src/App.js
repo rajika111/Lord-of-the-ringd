6984d156da1b2f28af75c4bbecc7016368f89de4
rajika111 <poower@gmail.com>
2019-10-28 13:25:44 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Movie from './Movie'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Movie title="The Return of the King" hours="3" minutes="21"></Movie>
+        <Movie title="The Fellowship of the Ring" hours="2" minutes="58"></Movie>
+        <Movie title="The Two Towers" hours="2" minutes="59"></Movie>
+        
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App; 
